87406b5a6a3a4d023f0f2f8a8f3a5ac15475a5ab
Tope <aluno81672@ipt.pt>
2022-06-08 23:53:34 +0100

    commit

---
+++ MyMath1/advanced/__init__.py
@@ -0,0 +1,2 @@
+from MyMath1.advanced.fib import *
+from MyMath1.advanced.sqrt import *
---
+++ MyMath1/arithmetic.py
@@ -0,0 +1,8 @@
+def add(n,x):
+    print(n+x)
+def div(n,x):
+    print(n/x)
+def mult(n,x):
+    print(n*x)
+def sub(n,x):
+    print(n-x)
